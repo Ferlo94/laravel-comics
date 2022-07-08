e81851d7ec88c040c595f09610aca11826b2e733
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-08 16:46:18 +0200

    footer add

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,42 @@
-<footer>
-    <h2>io sono il footer</h2>
+<footer class="footer">
+    <div class="footer__container">
+        <div class="footer__top">
+            <img src="{{asset('img/buy-comics-digital-comics.png')}}">
+            <p>PROVA</p>
+            <img src="{{asset('img/buy-comics-merchandise.png')}}">
+            <p>PROVA</p>
+            <img src="{{asset('img/buy-comics-subscriptions.png')}}">
+            <p>PROVA</p>
+            <img src="{{asset('img/buy-comics-shop-locator.png')}}">
+            <p>PROVA</p>
+            <img src="{{asset('img/buy-dc-power-visa.svg')}}">
+            <p>PROVA</p>
+        </div>
+    </div>
+    <div class="footer__mid">
+        <div class="footer__container">
+            <div class="footer__sx">
+                <div class="text-footer"><h3><strong class="white">DC COMICS</strong></h3><br><span class="grey">Lorem ipsum dolor sit amet consectetur</span></div>
+                <div class="text-footer"><h3><strong class="white">DC COMICS</strong></h3><br><span class="grey">Lorem ipsum dolor sit amet consectetur</span></div>
+                <div class="text-footer"><h3><strong class="white">DC COMICS</strong></h3><br><span class="grey">Lorem ipsum dolor sit amet consectetur</span></div>
+                <div class="text-footer"><h3><strong class="white">DC COMICS</strong></h3><br><span class="grey">Lorem ipsum dolor sit amet consectetur</span></div>
+            </div>
+            <div class="footer__dx"></div>
+        </div>
+    </div>
+    <div class="footer__bottom">
+        <div class="footer__container-1">
+
+            <h3 class="sign-up white">SIGN UP NOW!</h3>
+
+            <div class="follow-us">
+                <span><strong>FOLLOW US</strong></span>
+                <img src="../assets/img/footer-facebook.png" alt="">
+                <img src="../assets/img/footer-twitter.png" alt="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt="">
+            </div>
+        </div>    
+    </div>
 </footer>
