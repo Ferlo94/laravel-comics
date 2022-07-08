d7b28929e799b0a00d4f87d2ab4c0bbc3ad78bf0
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-08 16:54:07 +0200

    d

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -31,11 +31,11 @@
 
             <div class="follow-us">
                 <span><strong>FOLLOW US</strong></span>
-                <img src="../assets/img/footer-facebook.png" alt="">
+                {{-- <img src="../assets/img/footer-facebook.png" alt="">
                 <img src="../assets/img/footer-twitter.png" alt="">
                 <img src="../assets/img/footer-youtube.png" alt="">
                 <img src="../assets/img/footer-pinterest.png" alt="">
-                <img src="../assets/img/footer-periscope.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt=""> --}}
             </div>
         </div>    
     </div>
